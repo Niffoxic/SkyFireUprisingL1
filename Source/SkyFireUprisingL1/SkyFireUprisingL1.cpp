bb428d1d3925602c5814163234c29a1a7e6af9fd
Light it or Pass it <niffoisme@gmail.com>
2026-04-29 13:03:09 +0100

    Add project files.

---
+++ Source/SkyFireUprisingL1/SkyFireUprisingL1.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SkyFireUprisingL1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SkyFireUprisingL1, "SkyFireUprisingL1" );
